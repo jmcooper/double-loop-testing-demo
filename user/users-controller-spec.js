31b85ec441d062735a7cbb3866bff60b539c5804
Jim Cooper <jim-cooper@pluralsight.com>
2015-09-29 16:48:07 -0600

    Restore usersRepository after mocking

--- user/users-controller-spec.js
+++ user/users-controller-spec.js
@@ -16,6 +16,9 @@ describe('users api controller', function() {
       mockRequire('./users-repository', mockUsersRepository)
       usersController.get('123-abc')
     })
+    afterEach(function() {
+      sinon.restore(usersRepository)
+    })
     it('should get the user from the repository', function() {
       expect(mockUsersRepository.get.calledWith('123-abc')).toBeTruthy()
     })
