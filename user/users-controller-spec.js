77adddba781834f2fadead9189e89034106abd46
Jim Cooper <jim-cooper@pluralsight.com>
2015-09-29 16:06:53 -0600

    Failing test with mock

--- user/users-controller-spec.js
+++ user/users-controller-spec.js
@@ -1,7 +1,22 @@
 var usersController = require('./users-controller')
+var usersRepository = require('./users-repository')
+var mockRequire = require('mock-require')
+var mockUsersRepository
 
 describe('users api controller', function() {
   it('should have a get function', function() {
     expect(usersController.get).toBeTruthy();
+  })
+
+  describe('get', function() {
+    var result
+    beforeEach(function() {
+      mockUsersRepository = sinon.stub(usersRepository)
+      mockRequire.mock('./users-repository', mockUsersRepository)
+      usersController.get('123-abc')
+    })
+    it('should get the user from the repository', function() {
+      expect(usersRepository.get.calledWith('123-abc'))
+    })
   })
 })
