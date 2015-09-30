21dc3e4b1dd3f68ce951de20b6c1122816b539d5
Jim Cooper <jim-cooper@pluralsight.com>
2015-09-29 19:01:46 -0700

    Cleanup

--- user/users-controller.js
+++ user/users-controller.js
@@ -4,13 +4,7 @@ var q = require('q')
 module.exports = {
   get: function(id) {
     var deferred = q.defer()
-    var promise = usersRepository.get(id)
-    console.log(promise)
-    var user = promise
-      .then(function(userFromRepository){
-        console.log('**********')
-        deferred.resolve({id: userFromRepository.id})
-      }, function() {console.log('wat')})
+    usersRepository.get(id).then(function(user){ deferred.resolve({id: user.id}) })
     return deferred.promise
   }
 }
